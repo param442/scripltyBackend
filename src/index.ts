644ad4b8170047df29225f49e780f9196062cc08
Paramvir Singh <your-email@example.com>
2026-08-12 14:23:01 -0400

    test express startup 0.5

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-// import { toNodeHandler } from "better-auth/node";
-// import { auth } from "./lib/auth";
-import apiRoutes from "./routes";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+// import apiRoutes from "./routes";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -21,9 +21,9 @@ app.use(
 
 app.use(express.json());
 // Better Auth
-// app.all("/api/auth/*", toNodeHandler(auth));
+app.all("/api/auth/*", toNodeHandler(auth));
 
-app.use("/api", apiRoutes);
+// app.use("/api", apiRoutes);
 
 app.get("/api/health", (req, res) => {
   res.json({
